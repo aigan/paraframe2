9bc17bd8dd8cea6aaac8b30e4f8f04b9d3c4a68b
jonas <jonas>
2005-08-20 16:56:21 +0000

    moved user_login from paranormal and modified

---
+++ lib/Para/Frame/Action/user_login.pm
@@ -0,0 +1,70 @@
+#  $Id$  -*-perl-*-
+package Para::Frame::Action::user_login;
+#=====================================================================
+#
+# DESCRIPTION
+#   Paraframe user login action
+#
+# AUTHOR
+#   Jonas Liljegren   <jonas@paranormal.se>
+#
+# COPYRIGHT
+#   Copyright (C) 2004 Jonas Liljegren.  All Rights Reserved.
+#
+#   This module is free software; you can redistribute it and/or
+#   modify it under the same terms as Perl itself.
+#
+#=====================================================================
+
+use strict;
+
+use Para::Frame::Utils qw( throw passwd_crypt );
+
+sub handler
+{
+    my( $req ) = @_;
+
+    my $q = $req->q;
+
+    # Validation
+    #
+    my $username = $q->param('username')
+	or throw('incomplete', "Namn saknas\n");
+    my $password = $q->param('password') || "";
+    my $remember = $q->param('remember_login') || 0;
+
+    my @extra = ();
+    if( $remember )
+    {
+	push @extra, -expires => '+10y';
+    }
+
+    my $user_class = $Para::Frame::CFG->{'user_class'};
+    my $u = $user_class->get( $username );
+    $u or throw('validation', "Användaren $username existerar inte");
+    
+    $user_class->change_current_user( $u );
+
+    my $password_encrypted = passwd_crypt( $password );
+
+    if( $user_class->authenticate_user( $password_encrypted ) )
+    {
+	$req->cookies->add({
+	    'username' => $username,
+	    'password' => $password_encrypted,
+	},{
+	    @extra,
+	});
+
+	$q->delete('username');
+	$q->delete('password');
+
+	$req->run_hook('user_login', $u);
+
+	return "$username loggar in";
+    }
+
+    return "Inloggningen misslyckades\n";
+}
+
+1;
