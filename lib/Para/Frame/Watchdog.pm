9599fab503e1400df44650c3f091ce9d7c547134
jonas <jonas>
2006-01-17 19:46:08 +0000

    less debug

--- lib/Para/Frame/Watchdog.pm
+++ lib/Para/Frame/Watchdog.pm
@@ -144,7 +144,7 @@ sub check_process
 	my $usage = $cpu_delta/$sys_delta/10; # Get percent
 	$CPU_USAGE = ($CPU_USAGE * 2 + $usage ) / 3;
 
-	if( debug > 2 or $CPU_USAGE > 30 or $size > LIMIT_MEMORY_NOTICE )
+	if( debug > 3 or $CPU_USAGE > 30 or $size > LIMIT_MEMORY_NOTICE )
 	{
 	    debug sprintf( "Serverstat %.2d%% (%.2d%%) %5d MB",
 			   $usage, $CPU_USAGE, $size );
