be157f8f75f38b3a6e7f7e93f7919cf68490fc59
jonas <jonas>
2006-02-22 17:09:23 +0000

    bugfix

--- lib/Para/Frame.pm
+++ lib/Para/Frame.pm
@@ -1000,7 +1000,8 @@ sub add_background_jobs
 	# each background request
 
 	$req->{'original_request'} = $original_request;
-	$req->set_site($original_request->site);
+	$req->{'page'} = Para::Frame::Page->new();
+	$req->{'page'}->set_site($original_request->site);
 	$req->add_job('run_code', @$job);
 
 	for( my $i=0; $i<=$#BGJOBS_PENDING; $i++ )
