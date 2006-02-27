bae5dc03985b8ec1a626a5be0098d3def94c87d2
jonas <jonas>
2006-02-27 21:54:12 +0000

    Added a demoserv

---
+++ demo/demoserver.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+#  $Id$  -*-cperl-*-
+
+#=====================================================================
+#
+# DESCRIPTION
+#   General paraframe server (example)
+#
+# AUTHOR
+#   Jonas Liljegren   <jonas@paranormal.se>
+#
+# COPYRIGHT
+#   Copyright (C) 2005-2006 Avisita AB.  All Rights Reserved.
+#
+#=====================================================================
+
+our $APPROOT;
+our $PFROOT;
+our $WEBHOME;
+
+BEGIN
+{
+    our $VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+    print "Starting ritguides $VERSION\n";
+
+    use FindBin;
+    use Cwd 'abs_path';
+
+    $PFROOT = abs_path("$FindBin::Bin/../");
+    $APPROOT = $FindBin::Bin;
+    $WEBHOME = $ARGV[0] or die "Give the URL of the demo dir as an arg";
+}
+
+use strict;
+use locale;
+
+use lib "$PFROOT/lib";
+use lib "$APPROOT/lib";
+
+
+use Para::Frame;
+use Para::Frame::DBIx;
+use Para::Frame::Watchdog;
+use Para::Frame::Site;
+
+{
+    Para::Frame::Site->add({
+			    'code'        => 'demo',
+			    'webhome'     => $WEBHOME,
+			    'appbase'     => 'Para::Frame::Demo',
+			   });
+
+
+    my $cfg =
+    {
+     'paraframe' => $PFROOT,
+     'logfile'      => "/tmp/pfserver.log",
+     'pidfile'      => "/tmp/pfserver.pid",
+     'ttcdir'       => "/tmp",
+     'approot'      => $APPROOT,
+     'port'         => '7791',
+     'debug'        => $ARGV[1] || 0,
+    };
+    Para::Frame->configure( $cfg );
+
+
+    if( $ARGV[1] )
+    {
+        Para::Frame->daemonize(1);
+    }
+    else
+    {
+	Para::Frame->watchdog_startup();
+    }
+  }
+
+#########################################################
