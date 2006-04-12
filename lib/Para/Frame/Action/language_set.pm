3d7a3f9618a5ed296c8aed9be1f675265282fea7
jonas <jonas>
2006-04-12 13:56:44 +0000

    added action

---
+++ lib/Para/Frame/Action/language_set.pm
@@ -0,0 +1,50 @@
+#  $Id$  -*-perl-*-
+package Para::Frame::Action::language_set;
+#=====================================================================
+#
+# DESCRIPTION
+#   Paraframe language set action
+#
+# AUTHOR
+#   Jonas Liljegren   <jonas@paranormal.se>
+#
+# COPYRIGHT
+#   Copyright (C) 2006 Jonas Liljegren.  All Rights Reserved.
+#
+#   This module is free software; you can redistribute it and/or
+#   modify it under the same terms as Perl itself.
+#
+#=====================================================================
+
+use strict;
+
+use Para::Frame::Utils qw( throw passwd_crypt debug );
+
+sub handler
+{
+    my( $req ) = @_;
+
+    my $q = $req->q;
+
+    if( my $lc = $q->param('lang') )
+    {
+	$req->cookies->add({
+			    'lang' => $lc,
+			   });
+	return "Ändrade språket till $lc";
+    }
+    else
+    {
+	$req->cookies->remove('lang');
+	return "Tog bort språkval";
+    }
+}
+
+1;
+
+
+=head1 NAME
+
+Para::Frame::Action::user_login - For logging in
+
+=cut
