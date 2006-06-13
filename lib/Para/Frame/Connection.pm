b04d13bce7b978451795cb91e2737a6de765da41
jonas <jonas>
2006-06-13 16:26:52 +0000

    Cleanup. Now mostly works

--- lib/Para/Frame/Connection.pm
+++ lib/Para/Frame/Connection.pm
@@ -24,6 +24,8 @@ Para::Frame::Connection - Connection class for daemon IPC
 
 use strict;
 use Data::Dumper;
+use Carp qw( cluck confess );
+use POSIX;
 
 BEGIN
 {
@@ -64,20 +66,33 @@ sub new
 
     my $conn = bless
     {
-     socket => $sock,
-     host => $host_in,
+     socket     => $sock,
+     host       => $host_in,
+     io_select  => IO::Select->new($sock),
     }, $class;
+
+    $conn->reset_buffer;
 
     $conn->ping;
 
     return $conn;
 }
 
-sub dissconnect
+sub reset_buffer
 {
     my( $conn ) = @_;
 
-    $conn->{'socket'}->shutdown;
+    $conn->{inbuffer} = \"";
+    $conn->{datalength} = undef;
+}
+
+sub disconnect
+{
+    my( $conn ) = @_;
+
+    # I/we have stopped using this socket
+    $conn->reset_buffer;
+    $conn->{'socket'}->shutdown(2);
 }
 
 sub ping
@@ -85,6 +100,8 @@ sub ping
     my( $conn ) = @_;
 
     my $res = $conn->get_cmd_val("PING");
+
+    debug "Got $$res";
     return $res;
 }
 
@@ -92,7 +109,13 @@ sub get_cmd_val
 {
     my $conn = shift;
     $conn->send_code( @_ );
-    return Para::Frame::get_value( $Para::Frame::REQ );
+    my $res = $conn->get_value;
+    unless( $res )
+    {
+	confess "No result";
+    }
+
+    return $res;
 }
 
 sub send_code
@@ -104,14 +127,135 @@ sub send_code
 
 
     $valref ||= \ "1";
-    my $client = $Para::Frame::REQ->client;
+
     my $length = length($$valref) + length($code) + 1;
     my $socket = $conn->{'socket'};
 
-    debug 4, "Sending $length - $code - value";
+    debug "Sending $length - $code - value";
     unless( print $socket "$length\x00$code\x00" . $$valref )
     {
 	die "LOST CONNECTION while sending $code\n";
+    }
+}
+
+sub get_value
+{
+    my( $conn ) = @_;
+
+    # Either we know we have something to read
+    # or we are expecting an answer shortly
+
+    # This code is adapted from Para::Frame::get_value()
+
+    debug "Get value from $conn";
+
+
+    my $time      = time;
+    my $timeout   = 5;
+    my $io_select = $conn->{io_select};
+    my $socket    = $conn->{socket};
+    my $inbuffref = $conn->{inbuffref};
+
+  WAITE:
+    while(1)
+    {
+	foreach my $ready ( $io_select->can_read( $timeout ) )
+	{
+	    last WAITE if $ready == $socket;
+	}
+	if( time > $time + $timeout )
+	{
+	    warn "Data timeout!!!";
+
+	    cluck "trace for $socket";
+	    throw('action', "Data timeout while talking to client\n");
+	}
+    }
+
+    # Read data from client.
+    my $data='';
+    my $rv = $socket->recv($data,POSIX::BUFSIZ, 0);
+
+    debug "Read data...";
+
+    unless (defined $rv && length $data)
+    {
+	# EOF from client.
+	$conn->disconnect;
+	debug "End of file";
+	return undef;
+    }
+
+
+    $$inbuffref .= $data;
+    unless( $conn->{datalength} )
+    {
+	debug "Length of record?";
+	# Read the length of the data string
+	#
+	if( $$inbuffref =~ s/^(\d+)\x00// )
+	{
+	    debug "Setting length to $1";
+	    $conn->{datalength} = $1;
+	}
+	else
+	{
+	    debug "Strange INBUFFER content: $$inbuffref";
+
+	    $conn->disconnect;
+	    return undef;
+	}
+    }
+
+    my $datalength = $conn->{datalength};
+
+    unless( $datalength )
+    {
+	debug "No datalength";
+
+	$conn->reset_buffer;
+	return undef;
+    }
+
+    my $inbuffer_length = length $$inbuffref;
+    unless( $inbuffer_length == $datalength )
+    {
+	debug "The whole length not yet read ($inbuffer_length of $datalength)";
+	if( $inbuffer_length > $datalength )
+	{
+	    debug "Read too much";
+	    $conn->disconnect;
+	    return undef;
+	}
+
+	debug "Getting more...";
+	die "not implemented";
+    }
+
+
+    debug "The whole length read";
+
+    unless( $$inbuffref =~ s/^(\w+)\x00// )
+    {
+	debug "No code given: $$inbuffref";
+	$conn->reset_buffer;
+	return undef;
+    }
+
+    my( $code ) = $1;
+
+    if( $code eq 'RESP' )
+    {
+	debug "RESP recieved ($$inbuffref)";
+
+	$conn->reset_buffer;
+	return $inbuffref;
+    }
+    else
+    {
+	debug "Strange CODE: $code";
+	$conn->reset_buffer;
+	return undef;
     }
 }
 
