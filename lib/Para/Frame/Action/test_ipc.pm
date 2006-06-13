459a4c75c2be71e27b75fea9dc72af563eb1c3f0
jonas <jonas>
2006-06-13 17:41:05 +0000

    action for testing ipc

---
+++ lib/Para/Frame/Action/test_ipc.pm
@@ -0,0 +1,20 @@
+#  $Id$  -*-perl-*-
+package Para::Frame::Action::test_ipc;
+
+use strict;
+
+sub handler
+{
+    my( $req ) = @_;
+
+    my $daemon = "karl.rit.se:7791";
+
+    my $res = $req->send_to_daemon($daemon, 'run_action');
+
+    my $change = $req->change;
+    $change->note("Contacted $daemon: $res");
+
+    return "Test done";
+}
+
+1;
