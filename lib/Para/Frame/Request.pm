0228ec39c512f8e67473e1d78b3abd99dd2c654d
jonas <jonas>
2006-08-21 09:04:44 +0000

    fixed copyright

--- lib/Para/Frame/Request.pm
+++ lib/Para/Frame/Request.pm
@@ -9,7 +9,7 @@ package Para::Frame::Request;
 #   Jonas Liljegren   <jonas@paranormal.se>
 #
 # COPYRIGHT
-#   Copyright (C) 2004 Jonas Liljegren.  All Rights Reserved.
+#   Copyright (C) 2004-2006 Jonas Liljegren.  All Rights Reserved.
 #
 #   This module is free software; you can redistribute it and/or
 #   modify it under the same terms as Perl itself.
