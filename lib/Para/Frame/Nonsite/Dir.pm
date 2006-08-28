8ff4332d532d746cc105436dbe686ffdfdd2dc57
jonas <jonas>
2006-08-28 06:42:04 +0000

    New filed

---
+++ lib/Para/Frame/Nonsite/Dir.pm
@@ -0,0 +1,43 @@
+#  $Id$  -*-cperl-*-
+package Para::Frame::Nonsite::Dir;
+#=====================================================================
+#
+# DESCRIPTION
+#   Paranormal.se framework Dir class
+#
+# AUTHOR
+#   Jonas Liljegren   <jonas@paranormal.se>
+#
+# COPYRIGHT
+#   Copyright (C) 2006 Jonas Liljegren.  All Rights Reserved.
+#
+#   This module is free software; you can redistribute it and/or
+#   modify it under the same terms as Perl itself.
+#
+#=====================================================================
+
+=head1 NAME
+
+Para::Frame::Nonsite::Dir - Represents a dir
+
+=cut
+
+use strict;
+
+BEGIN
+{
+    our $VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+    print "Loading ".__PACKAGE__." $VERSION\n";
+}
+
+use base qw( Para::Frame::Dir Para::Frame::File );
+
+#######################################################################
+
+1;
+
+=head1 SEE ALSO
+
+L<Para::Frame>
+
+=cut
---
+++ lib/Para/Frame/Nonsite/Page.pm
@@ -0,0 +1,43 @@
+#  $Id$  -*-cperl-*-
+package Para::Frame::Nonsite::Page;
+#=====================================================================
+#
+# DESCRIPTION
+#   Paranormal.se framework Page class
+#
+# AUTHOR
+#   Jonas Liljegren   <jonas@paranormal.se>
+#
+# COPYRIGHT
+#   Copyright (C) 2006 Jonas Liljegren.  All Rights Reserved.
+#
+#   This module is free software; you can redistribute it and/or
+#   modify it under the same terms as Perl itself.
+#
+#=====================================================================
+
+=head1 NAME
+
+Para::Frame::Nonsite::Page - Represents a tt page
+
+=cut
+
+use strict;
+
+BEGIN
+{
+    our $VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+    print "Loading ".__PACKAGE__." $VERSION\n";
+}
+
+use base qw( Para::Frame::Page Para::Frame::File );
+
+#######################################################################
+
+1;
+
+=head1 SEE ALSO
+
+L<Para::Frame>
+
+=cut
