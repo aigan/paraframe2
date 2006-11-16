ac184b0cda998bf7f4b1874a76fa712d73e35fdf
jonas <jonas>
2006-11-16 05:03:18 +0000

    Added debug on site mismatch

--- lib/Para/Frame/Site.pm
+++ lib/Para/Frame/Site.pm
@@ -9,7 +9,7 @@ package Para::Frame::Site;
 #   Jonas Liljegren   <jonas@paranormal.se>
 #
 # COPYRIGHT
-#   Copyright (C) 2004 Jonas Liljegren.  All Rights Reserved.
+#   Copyright (C) 2004-2006 Jonas Liljegren.  All Rights Reserved.
 #
 #   This module is free software; you can redistribute it and/or
 #   modify it under the same terms as Perl itself.
@@ -431,6 +431,9 @@ sub uri2file
 	my $args = {};
 	$args->{'site'} = $site;
 	debug "Getting uri2file from SUBREQUEST";
+	debug "This site is $site : ".$site->code;
+	debug "Req  site is $req_site : ".$req_site->code;
+
 	return $req->new_subrequest($args,
 				    \&Para::Frame::Request::uri2file,
 				    @_ );
