46f4d61b10fd999705fb524d9ab68e49b125fb18
jonas <jonas>
2007-09-04 21:40:35 +0000

    Made URI a wrapper class

--- lib/Para/Frame/URI.pm
+++ lib/Para/Frame/URI.pm
@@ -24,6 +24,8 @@ Para::Frame::URI - Represent an URI
 
 use strict;
 
+use URI;
+
 BEGIN
 {
     our $VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
@@ -31,28 +33,527 @@ BEGIN
 }
 
 
+=head1 DESCRIPTION
+
+Represents an URI. This is a wrapper for L<URI> and L<URI::QueryParam>
+that redirects calls to those classes. This class is extendable by
+subclasses and it's integrated with L<Para::Frame::File>.
+
+Methods not encapsulated are:
+
+  as_string : use plain()
+  eq        : use equals()
+
+=cut
+
+
 #######################################################################
 
-=head2 new
+=head3 new
 
-  URI->new($uri, $scheme)
-
-Returns a L<URI> object, but may be modified to suit paraframe.
+Wrapper for L<URI/new>
 
 =cut
 
 sub new
 {
-    my( $class, $uri, $scheme ) = @_;
+    my( $class ) = shift;
 
-    $uri = defined ($uri) ? "$uri" : "";   # stringify
+    my $uri = URI->new(@_);
 
-    # Remove double / in path
-
-    $uri =~ s!([^:])//+!$1/!g;
-
-    return URI->new($uri, $scheme);
+    return bless
+    {
+     value => $uri,
+    }, $class;
 }
+
+
+#######################################################################
+
+=head2 as_html
+
+  $a->as_html
+
+=cut
+
+sub as_html
+{
+    my( $url, $label ) = @_;
+
+    return "" unless $url->{'value'};
+
+    my $href = $url->{'value'}->as_string;
+    $label ||= $href;
+
+    my $label_out = CGI->escapeHTML($label);
+    my $href_out = CGI->escapeHTML($href);
+
+    return "<a href=\"$href_out\">$label_out</a>";
+}
+
+
+#######################################################################
+
+=head2 sysdesig
+
+  $a->sysdesig()
+
+The designation of an object, to be used for node administration or
+debugging.
+
+=cut
+
+sub sysdesig
+{
+    if( my $str = "$_[0]->{value}" )
+    {
+	return "URL $str";
+    }
+    else
+    {
+	return "URL undef";
+    }
+}
+
+
+#######################################################################
+
+=head2 desig
+
+  $a->desig()
+
+The designation of an object, to be used for node administration or
+debugging.
+
+=cut
+
+sub desig
+{
+    return "$_[0]->{value}";
+}
+
+
+#######################################################################
+
+=head3 new_abs
+
+See L<URI/new_abs>
+
+=cut
+
+sub new_abs
+{
+    my( $class ) = shift;
+
+    my $uri = URI->new_abs(@_);
+
+    return bless
+    {
+     value => $uri,
+    }, $class;
+}
+
+
+#######################################################################
+
+=head3 clone
+
+See L<URI/clone>
+
+=cut
+
+sub clone
+{
+    my $uri = $_[0]->clone;
+    my $class = ref $_[0];
+
+    return bless
+    {
+     value => $uri,
+    }, $class;
+}
+
+
+#######################################################################
+
+=head3 getset
+
+Used by most get/set wrapper methods
+
+=cut
+
+sub getset
+{
+    my( $u, $method ) = (shift, shift);
+    if( my $uri = $u->{'value'} )
+    {
+	return $uri->$method(@_);
+    }
+
+    return "";
+}
+
+
+#######################################################################
+
+=head3 schema
+
+See L<URI/schema>
+
+=cut
+
+sub schema
+{
+    return shift->getset('schema',@_);
+}
+
+
+#######################################################################
+
+=head3 opaque
+
+See L<URI/opaque>
+
+=cut
+
+sub opaque
+{
+    return shift->getset('opaque',@_);
+}
+
+
+#######################################################################
+
+=head3 path
+
+See L<URI/path>
+
+=cut
+
+sub path
+{
+    return shift->getset('path',@_);
+}
+
+
+#######################################################################
+
+=head3 fragment
+
+See L<URI/fragment>
+
+=cut
+
+sub fragment
+{
+    return shift->getset('fragment',@_);
+}
+
+
+#######################################################################
+
+=head3 canonical
+
+See L<URI/canonical>
+
+=cut
+
+sub canonical
+{
+    my $uri = $_[0]->canonical;
+
+    if( $_[0]->eq( $uri ) )
+    {
+	return $_[0];
+    }
+
+    my $class = ref $_[0];
+    return bless
+    {
+     value => $uri,
+    }, $class;
+}
+
+
+#######################################################################
+
+=head3 abs
+
+See L<URI/abs>
+
+=cut
+
+sub abs
+{
+    my $uri = $_[0]->abs($_[1]);
+
+    if( $_[0]->eq( $uri ) )
+    {
+	return $_[0];
+    }
+
+    my $class = ref $_[0];
+    return bless
+    {
+     value => $uri,
+    }, $class;
+}
+
+
+#######################################################################
+
+=head3 rel
+
+See L<URI/rel>
+
+=cut
+
+sub rel
+{
+    my $uri = $_[0]->rel($_[1]);
+
+    if( $_[0]->eq( $uri ) )
+    {
+	return $_[0];
+    }
+
+    my $class = ref $_[0];
+    return bless
+    {
+     value => $uri,
+    }, $class;
+}
+
+
+#######################################################################
+
+=head3 authority
+
+See L<URI/authority>
+
+=cut
+
+sub authority
+{
+    return shift->getset('authority',@_);
+}
+
+
+#######################################################################
+
+=head3 path_query
+
+See L<URI/path_query>
+
+=cut
+
+sub path_query
+{
+    return shift->getset('path_query',@_);
+}
+
+
+#######################################################################
+
+=head3 path_segments
+
+See L<URI/path_segments>
+
+=cut
+
+sub path_segments
+{
+    return shift->getset('path_segments',@_);
+}
+
+
+#######################################################################
+
+=head3 query
+
+See L<URI/query>
+
+=cut
+
+sub query
+{
+    return shift->getset('query',@_);
+}
+
+
+#######################################################################
+
+=head3 query_form
+
+See L<URI/query_form>
+
+=cut
+
+sub query_form
+{
+    return shift->getset('query_form',@_);
+}
+
+
+#######################################################################
+
+=head3 query_keywords
+
+See L<URI/query_keywords>
+
+=cut
+
+sub query_keywords
+{
+    return shift->getset('query_keywords',@_);
+}
+
+
+#######################################################################
+
+=head3 userinfo
+
+See L<URI/userinfo>
+
+=cut
+
+sub userinfo
+{
+    return shift->getset('userinfo',@_);
+}
+
+
+#######################################################################
+
+=head3 host
+
+See L<URI/host>
+
+=cut
+
+sub host
+{
+    return shift->getset('host',@_);
+}
+
+
+#######################################################################
+
+=head3 port
+
+See L<URI/port>
+
+=cut
+
+sub port
+{
+    return shift->getset('port',@_);
+}
+
+
+#######################################################################
+
+=head3 host_port
+
+See L<URI/host_port>
+
+=cut
+
+sub host_port
+{
+    return shift->getset('host_port',@_);
+}
+
+
+#######################################################################
+
+=head3 default_port
+
+See L<URI/default_port>
+
+=cut
+
+sub default_port
+{
+    return $_[0]->{'value'}->default_port;
+}
+
+
+#######################################################################
+
+=head3 getset_query
+
+Used by query get/set wrapper methods
+
+=cut
+
+sub getset_query
+{
+    my( $u, $method ) = (shift, shift);
+    if( my $uri = $u->{'value'} )
+    {
+	return $uri->$method(@_);
+    }
+
+    return "";
+}
+
+
+#######################################################################
+
+=head3 query_param
+
+See L<URI/query_param>
+
+=cut
+
+sub query_param
+{
+    return shift->getset_query('query_param',@_);
+}
+
+
+#######################################################################
+
+=head3 query_param_append
+
+See L<URI/query_param_append>
+
+=cut
+
+sub query_param_append
+{
+    return shift->getset_query('query_param_append',@_);
+}
+
+
+#######################################################################
+
+=head3 query_param_delete
+
+See L<URI/query_param_delete>
+
+=cut
+
+sub query_param_delete
+{
+    return shift->getset('query_param_delete',@_);
+}
+
+
+#######################################################################
+
+=head3 query_form_hash
+
+See L<URI/query_form_hash>
+
+=cut
+
+sub query_form_hash
+{
+    return shift->getset('query_form_hash',@_);
+}
+
 
 #######################################################################
 
@@ -65,6 +566,6 @@ Jonas Liljegren E<lt>jonas@paranormal.seE<gt>
 
 =head1 SEE ALSO
 
-L<Para::Frame>, L<URI>
+L<Para::Frame>, L<URI>, L<URI::QueryParam>, L<Para::Frame::File>
 
 =cut
