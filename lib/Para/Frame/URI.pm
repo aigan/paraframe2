2924bf78677b638c63543dc9f24f5ff59bd24a02
jonas <jonas>
2008-10-23 23:36:07 +0000

    layout

--- lib/Para/Frame/URI.pm
+++ lib/Para/Frame/URI.pm
@@ -87,6 +87,12 @@ sub as_html
     my $href = $url->{'value'}->as_string;
     $label ||= $href;
 
+    $label =~ s/^https?:\/\///;
+    if( $url->path eq '/' )
+    {
+	$label =~ s/\/$//;
+    }
+
     my $label_out = CGI->escapeHTML($label);
     my $href_out = CGI->escapeHTML($href);
 
