e2728542bc299f60f8e75cbd779c94ac1d35bf4c
Jonas Liljegren <jonas@liljegren.org>
2013-04-22 11:47:10 +0200

    Added table row highlightning

--- share/html/pf/js/pf.js
+++ share/html/pf/js/pf.js
@@ -25,3 +25,27 @@ function log(stuff)
         console.log(stuff);
     }
 }
+
+
+(function($) {
+
+    function pf_document_ready()
+    {
+	$('table.markrow input[type="checkbox"]').change(function(){
+	    log('Checkbox highlight toggle');
+            if ($(this).is(':checked')){
+		$(this).parent().addClass('highlighted');
+		$(this).parent().siblings().addClass('highlighted');
+            } else if($(this).parent().is('.highlighted')) {
+		$(this).parent().removeClass('highlighted');
+		$(this).parent().siblings().removeClass('highlighted');
+            }
+	});
+    };
+    
+    
+    jQuery(document).ready(pf_document_ready);
+}
+)(jQuery);
+
+log('PF js loaded');
